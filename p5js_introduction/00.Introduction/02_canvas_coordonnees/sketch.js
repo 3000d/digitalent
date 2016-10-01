0ba8c79331819fcbebe7dac96cbb7ed10b859952
Greg Berger <greg@3kd.be>
2016-10-02 01:51:10 +0200

    ajout de cercles à différentes positions pour l'exemple

--- p5js_introduction/00.Introduction/02_canvas_coordonnees/sketch.js
+++ p5js_introduction/00.Introduction/02_canvas_coordonnees/sketch.js
@@ -8,4 +8,20 @@ function setup() {
   // c'est la valeur `0,0` => 0 pixel en largeur; 0 pixels en hauteur
 
   ellipse(0,0, 100,100); // dessine un cercle de 100 pixels de diamètre dont le centre est à 0,0
+
+  ellipse(0, 250, 100, 100);
+
+  ellipse(0,500, 100, 100);
+
+  ellipse(400, 0, 100, 100);
+
+  ellipse(400, 250, 100, 100);
+
+  ellipse(400, 500, 100, 100);
+
+  ellipse(800, 0, 100, 100);
+
+  ellipse(800, 250, 100, 100);
+
+  ellipse(800, 500, 100, 100);
 }
