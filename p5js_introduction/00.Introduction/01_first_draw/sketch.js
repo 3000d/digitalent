81639d27fa8cb6cd06f9696baaad0a0e627503b9
Greg Berger <greg@3kd.be>
2016-10-02 01:26:25 +0200

    ajout des commentaires

--- p5js_introduction/00.Introduction/01_first_draw/sketch.js
+++ p5js_introduction/00.Introduction/01_first_draw/sketch.js
@@ -1,7 +1,12 @@
+/*
+* la fonction setup est celle dans laquelle nous allons
+* définir les paramètres de départ de notre sketch
+*/
 function setup() {
   // dessiner un cercle dans p5js est aussi simple que cela
   ellipse(50,50,10,10);
 
   // pour dessiner un carré ou un rectangle
   rect(70,80, 10,10);
+
 }
