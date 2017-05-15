48dfc96dffd6f2503cb1caecb896d7c13614e135
Greg Berger <greg@3kd.be>
2017-05-15 20:54:57 +0200

    Ajout documentation arduino

--- p5js_introduction/examples/shape_morpher/sketch.js
+++ p5js_introduction/examples/shape_morpher/sketch.js
@@ -1,9 +1,15 @@
 function setup() {
   createCanvas(800,600);
+  console.log('hello');
+  console.log(frameCount);
 }
 
 function draw() {
-  
-  
-  
+
+    console.log(frameCount)
+
+}
+
+function mouseReleased(){
+  setup();
 }
